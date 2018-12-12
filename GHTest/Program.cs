dc6f80825c73933f1280b0e6361fa8f94809ba94
Leonid Kononplin <konoplin.7777@gmail.com>
2018-12-12 17:31:46 +0300

    Тест готов

---
+++ GHTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace GHTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub!!!");
+            Thread.Sleep(1000);
+            Console.Write("\a");
+            Thread.Sleep(3000);
+        }
+    }
+}
